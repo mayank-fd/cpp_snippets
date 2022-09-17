e4bb6c46f992aa1a3ee92fc88bbddb1243be8f9f
Mayank-Vishnoi <86844747+Mayank-Vishnoi@users.noreply.github.com>
2022-09-17 18:10:51 +0530

    Update stress_testing.cpp

--- Competitive Programming/stress_testing.cpp
+++ Competitive Programming/stress_testing.cpp
@@ -1,9 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-int rand(int l, int r){
+int rand (int l, int r) {
    uniform_int_distribution<int> uid(l, r);
    return uid(rng);
 }
@@ -16,6 +22,7 @@ string s, t;
 struct state () {
    
 };
+state res1, res2;
 
 bool checker (state &a, state &b) {
    return true;
@@ -32,14 +39,14 @@ state correct_solution () {
 }
 
 // Random n numbers between l and r
-void num(int l, int r, int n) {
+void num (int l, int r, int n) {
    for (int i = 0; i < n; ++i) {
       a.push_back(rand(l, r));
    }
 }
 
 // Random n strings of length l
-void str(int l, int n) {
+void str (int l, int n) {
    for (int i = 0; i < n; ++i) {
       string tmp = "";
       for (int j = 0; j < l; ++j) {
@@ -61,6 +68,7 @@ int main() {
    ios_base::sync_with_stdio(!cin.tie(0););
    n = rand(1, N);
    for (int i = 1; ; i++) {
+      dbg(i);
       res1 = your_solution(), res2 = correct_solution();
       if (!checker) {
          // dbg here
