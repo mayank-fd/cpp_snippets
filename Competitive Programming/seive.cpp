4f4d6a82073b6706f9c31fa2a2266d52b330eedd
Mayank-Vishnoi <86844747+Mayank-Vishnoi@users.noreply.github.com>
2022-10-01 16:55:14 +0530

    Update seive.cpp

--- Competitive Programming/seive.cpp
+++ Competitive Programming/seive.cpp
@@ -1,10 +1,20 @@
-const int N = 100;
-vector<bool> is_prime(101, true);
-is_prime[0] = is_prime[1] = false;
-for (int i = 2; i * i <= N; i++) {
-   if (is_prime[i]) {
-      for (int p = i * i; p <= N; p += i) {
-         is_prime[p] = false;
-      }
-   }
+const int N = 1e6;
+vector<bool> is_prime;
+vector<int> primes;
+
+void sieve () {
+  is_prime.assign(N + 1, true);
+  is_prime[0] = is_prime[1] = false;
+  for (int i = 2; i * i <= N; i++) {
+     if (is_prime[i]) {
+        for (int p = i * i; p <= N; p += i) {
+           is_prime[p] = false;
+        }
+     }
+  }
+  for (int i = 1; i <= N; i++) {
+    if (is_prime[i]) {
+      primes.push_back(i);
+    }
+  }
 }
